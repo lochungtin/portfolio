44b30d77f5c9a7d51983ac6469f0fc5b0d1b2f0e
lochungtin <lochungtin@gmail.com>
2022-07-18 15:45:23 +0100

    mobile header

--- pages/components/header/index.js
+++ pages/components/header/index.js
@@ -2,24 +2,48 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import styles from './header.module.css';
+import useMediaQuery from '../utils/media';
+import { useState } from 'react';
 
 export default function Header() {
+    const [expanded, toggleExpanded] = useState(false);
+    const isMobile = useMediaQuery();
+    const showItems = isMobile == expanded;
+
+    const toggle = () => toggleExpanded(!expanded);
+
     return (
         <header className={styles.header}>
+            {isMobile ? <button className={styles.menu} onClick={toggle}>
+                <Image src='/icons/menu.svg' height={30} width={30} alt='menu' />
+            </button> : null}
+            {isMobile ? <div className={styles.logo}>
+                <Link href='/'>
+                    <Image src='/logo/logo_full_c.svg' height={50} width={200} alt='logo' />
+                </Link>
+            </div> : null}
             <Link href='#about'>
-                <a className={styles.headerLink}>ABOUT</a>
+                <a className={`${styles.headerLink} ${showItems ? '' : styles.hidden}`}>
+                    {showItems ? 'ABOUT' : ''}
+                </a>
             </Link>
             <Link href='#education'>
-                <a className={styles.headerLink}>EDUCATION</a>
+                <a className={`${styles.headerLink} ${showItems ? '' : styles.hidden}`}>
+                    {showItems ? 'EDUCATION' : ''}
+                </a>
             </Link>
-            <Link href='/'>
+            {!isMobile ? <Link href='/'>
                 <Image src='/logo/logo_full_c.svg' height={50} width={200} alt='logo' />
-            </Link>
+            </Link> : null}
             <Link href='#projects'>
-                <a className={styles.headerLink}>PROJECTS</a>
+                <a className={`${styles.headerLink} ${showItems ? '' : styles.hidden}`}>
+                    {showItems ? 'PROJECTS' : ''}
+                </a>
             </Link>
             <Link href='#achievements'>
-                <a className={styles.headerLink}>ACHIEVEMENTS</a>
+                <a className={`${styles.headerLink} ${showItems ? '' : styles.hidden}`}>
+                    {showItems ? 'ACHIEVEMENTS' : ''}
+                </a>
             </Link>
         </header>
     );
---
+++ pages/components/utils/media.js
@@ -0,0 +1,23 @@
+import { useState, useCallback, useEffect } from 'react';
+
+const mobile = 600;
+
+export default function useMediaQuery(width = mobile) {
+    const [targetReached, setTargetReached] = useState(false);
+
+    const updateTarget = useCallback(e => {
+        setTargetReached(e.matches * true);
+    }, []);
+
+    useEffect(() => {
+        const media = window.matchMedia(`(max-width: ${width}px)`)
+        media.addEventListener('change', e => updateTarget(e))
+
+        if (media.matches)
+            setTargetReached(true)
+
+        return () => media.removeEventListener('change', e => updateTarget(e))
+    }, [width, updateTarget]);
+
+    return targetReached;
+};

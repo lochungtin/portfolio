3d24557a5760576436069a1f1d8deda07de1a4c7
lochungtin <lochungtin@gmail.com>
2022-07-19 15:07:20 +0100

    cheap fix

--- pages/components/skilldisplay/index.js
+++ pages/components/skilldisplay/index.js
@@ -2,18 +2,19 @@ import Image from 'next/image';
 import styles from './skilldisplay.module.css';
 
 export default function SkillDisplay({ data }) {
-    const name = data.name || '';
+    if (!data)
+        return <div></div>;
 
-    const src = `/icons/tech/${name.toLowerCase()}.svg`;
+    const src = `/icons/tech/${data.name.toLowerCase()}.svg`;
     return (
         <div className={styles.spacer}>
             <div className={styles.root}>
-                <Image src={src} width={20} height={20} alt={name} />
-                <p>{name}</p>
+                <Image src={src} width={20} height={20} alt={data.name} />
+                <p>{data.name}</p>
                 <div className={styles.dotContainer}>
                     {(new Array(data.rating).fill(1)).map((d, i) => <div className={styles.marker} key={i}></div>)}
                 </div>
             </div>
         </div>
-    )
+    );
 }

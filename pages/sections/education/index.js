0e1ed60f8faec83d24a7f65a247861c744b4a72f
lochungtin <lochungtin@gmail.com>
2022-07-19 19:03:02 +0100

    education section

--- pages/sections/education/index.js
+++ pages/sections/education/index.js
@@ -4,6 +4,54 @@ import BinarySelector from '../../components/binaryselector';
 
 import styles from './education.module.css';
 
+const formal = [
+    {
+        title: 'MRes AI and ML',
+        location: `Imperial College London`,
+        time: '2022 - Current',
+        score: 'Pending',
+    },
+    {
+        title: 'BSc Computer Science',
+        location: `King's College London`,
+        time: '2019 - 2022',
+        score: '79 (First Class Honours)',
+    },
+    {
+        title: `IBDP`,
+        location: 'HKCCCU Logos Academy',
+        time: '2017 - 2019',
+        score: '36/45',
+    },
+]
+
+const online = [
+    {
+        title: 'Reinforcement Learning Specialisation',
+        location: 'University of Alberta',
+        time: '2021 Summer',
+        score: 'Certificate of Participation',
+    },
+    {
+        title: 'Intro to TensorFlow for Deep Learning',
+        location: 'TensorFlow',
+        time: '2021 Summer',
+        score: 'n/a',
+    },
+    {
+        title: `CS50's Mobile App Development with React Native`,
+        location: 'Harvard University',
+        time: '2020 Summer',
+        score: 'Certificate of Participation',
+    },
+    {
+        title: 'Neural Networks and Deep Learning',
+        location: 'deeplearning.ai',
+        time: '2020 Summer',
+        score: 'Certificate of Participation',
+    },
+]
+
 export default function Education() {
     const [selection, setSelection] = useState(1);
 
@@ -11,15 +59,25 @@ export default function Education() {
         <section id='education'>
             <p className={styles.title}>Education</p>
             <BinarySelector
-                val0='Online Courses'
-                val1='Formal Education'
                 selection={selection}
+                val0='Online Courses' val1='Formal Education'
                 onClick={() => setSelection((selection + 1) % 2)}
             />
-            {selection ? <div>
-
-            </div> : <div>
-            </div>}
+            {(selection ? formal : online).map((data, index) => {
+                return <div className={styles.educationitemBox} key={index}>
+                    <div className={styles.horizontalP}>
+                        <p className={styles.horizontalEducationTitle}>
+                            <span className={styles.educationTitle}>{data.title}</span>
+                            {` @ ${data.location}`}
+                        </p>
+                        <p className={styles.eductationTimeText}><span>({data.time})</span></p>
+                    </div>
+                    <p className={styles.educationScoreText}>
+                        {'Qualification: '}
+                        <span className={styles.educationScoreValText}>{data.score}</span>
+                    </p>
+                </div>
+            })}
         </section>
     );
 }

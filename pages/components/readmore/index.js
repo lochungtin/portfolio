fc5207a384743c45f897a6df5a16f7df301796d7
lochungtin <lochungtin@gmail.com>
2022-07-19 23:36:23 +0100

    hopefully fix

--- pages/components/readmore/index.js
+++ pages/components/readmore/index.js
@@ -5,7 +5,7 @@ import styles from './readmore.module.css';
 
 export default function ReadMore({ href }) {
     return (
-        <Link href={href}>
+        <Link href={href || ''}>
             <a className={styles.root}>
                 <p className={styles.text}>Read More</p>
                 <Image src='/icons/chevron-right.svg' width={20} height={20} alt='right' />

4296eec56a727531607e6677dbd1386f85b9687c
lochungtin <lochungtin@gmail.com>
2022-08-03 14:15:38 +0100

    style tags

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -10,8 +10,23 @@ import styles from '../../styles/home.module.css';
 import layout from '../../styles/layout.module.css';
 import text from '../../styles/text.module.css';
 
+const tags = {
+	0: 'Web Apps',
+	1: 'AI and ML',
+	2: 'Algorithms',
+	3: 'Microcontrollers',
+};
+
+let filter = {};
+Object.keys(tags).forEach((key) => {
+	filter[key] = false;
+});
+
+const data = {};
+
 export default function Projects() {
 	const [textFilter, setTextFilter] = useState('');
+	const [tagFilter, setTagFilter] = useState(filter);
 	const [expanded, setExpanded] = useState(false);
 	const isMobile = useMediaQuery();
 
@@ -19,9 +34,16 @@ export default function Projects() {
 
 	const clearSearch = () => setTextFilter('');
 
+	const toggleExpanded = () => setExpanded(!expanded);
+
 	const handleTextChange = (event) => setTextFilter(event.target.value);
 
-	const toggleExpanded = () => setExpanded(!expanded);
+	const handleLabelToggle = (key) =>
+		setTagFilter(() => {
+			let next = { ...tagFilter };
+			next[key] = !next[key];
+			return next;
+		});
 
 	return (
 		<div className={styles.container}>
@@ -55,26 +77,18 @@ export default function Projects() {
 				<div className={styles.horizontalSplit}>
 					{showFilter ? (
 						<div className={styles.tagList}>
-							<label className={styles.checkmarkContainer}>
-								One
-								<input type='checkbox' name='death' />
-								<span className={styles.checkmark}></span>
-							</label>
-							<label className={styles.checkmarkContainer}>
-								One
-								<input type='checkbox' name='death' />
-								<span className={styles.checkmark}></span>
-							</label>
-							<label className={styles.checkmarkContainer}>
-								One
-								<input type='checkbox' name='death' />
-								<span className={styles.checkmark}></span>
-							</label>
-							<label className={styles.checkmarkContainer}>
-								One
-								<input type='checkbox' name='death' />
-								<span className={styles.checkmark}></span>
-							</label>
+							{Object.entries(tags).map(([key, tag], index) => (
+								<label
+									className={styles.checkmarkContainer}
+									key={index}
+									onChange={() => handleLabelToggle(key)}>
+									<div className={styles.tagFilterTag}>
+										<p className={styles.filterTagText}>{tag}</p>
+									</div>
+									<input type='checkbox' checked={tagFilter[key]} />
+									<span className={styles.checkmark}></span>
+								</label>
+							))}
 						</div>
 					) : null}
 					<div className={styles.projectList}>

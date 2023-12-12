ecb23c654a52d798133b99415e77438d57506bfd
lochungtin <lochungtin@gmail.com>
2023-12-12 21:44:13 +0000

    education cards

--- src/app/cards.tsx
+++ src/app/cards.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
+import education from './education.module.css';
 import skills from './skill.module.css';
 
-export const SkillCard = ({ icon, name, score }: { icon: string, name: string, score: string }) => {
+export const SkillCard = ({
+    icon, name, score
+}: {
+    icon: string, name: string, score: string
+}) => {
     return (
         <div className={skills.card}>
             <div className={skills.top}>
@@ -9,6 +14,36 @@ export const SkillCard = ({ icon, name, score }: { icon: string, name: string, s
                 <p>{name}</p>
             </div>
             <div className={skills.indicator + " " + skills["i" + score]}></div>
+        </div>
+    );
+}
+
+export const EducationCard = ({
+    name, loc, start, end, grade, isLeft
+}: {
+    name: string, loc: string, start: string, end: string, grade: string, isLeft: boolean
+}) => {
+    let splt: string[] = name.split(" ");
+    splt.reverse();
+    const title = splt.pop();
+    splt.reverse();
+    const name_s = splt.join(" ");
+
+    return (
+        <div className={education.card}>
+            <div className={education.cover + " " + (isLeft ? education.left : education.right)}></div>
+            <div className={education.border}>
+                <div className={education.content}>
+                    <div className={education.top}>
+                        <p><span>{title}</span> {name_s}</p>
+                        <p className={education.location}>{loc}</p>
+                    </div>
+                    <div className={education.details}>
+                        <p>{start} - {end}</p>
+                        <p>{grade}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { SkillCard } from "./cards";
+import { EducationCard, SkillCard } from "./cards";
 import layout from './layout.module.css';
 
 const Skills = () => {
@@ -30,10 +30,56 @@ const Skills = () => {
 	);
 }
 
+const Projects = () => {
+
+}
+
+const Education = () => {
+	return (
+		<section>
+			<h1><span>EDU</span>CATION</h1>
+
+			<EducationCard
+				name="DPhil Medical Physics and Bioengineering"
+				loc="University College London"
+				start="2023"
+				end="Current"
+				grade="Pending"
+				isLeft={true}
+			/>
+			<EducationCard
+				name="MRes AI & ML"
+				loc="Imperial College London"
+				start="2023"
+				end="2022"
+				grade="Distinction"
+				isLeft={false}
+			/>
+			<EducationCard
+				name="BSc Computer Science"
+				loc="King's College London"
+				start="2022"
+				end="2019"
+				grade="First Class Honours"
+				isLeft={true}
+			/>
+			<EducationCard
+				name="IB Diploma Program"
+				loc="HKCCCU Logos Academy"
+				start="2017"
+				end="2019"
+				grade="36 / 45"
+				isLeft={false}
+			/>
+		</section>
+	);
+}
+
 export default function Home() {
 	return (
 		<main>
 			<Skills />
+			<Education />
 		</main>
 	)
 }

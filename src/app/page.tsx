07cb50ae2a92d85d694ec87db11ba028bd52bf8f
lochungtin <lochungtin@gmail.com>
2023-12-23 22:53:43 +0000

    pc styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,61 @@
 import Image from 'next/image';
+import about from './about.module.css';
+import badge from './badge.png';
 import { AchievementCard, EducationCard, ProjectCard, SkillCard } from "./cards";
 import layout from './layout.module.css';
+
+const About = ({ name }: { name: string }) => {
+	return (
+		<section>
+			<div className={about.badge}>
+				<div className={about.overlay}></div>
+				<div className={about.image} style={{ backgroundImage: `url(${badge.src})` }} />
+				<div className={about.content}>
+					<Image className={about.icon} src="/assets/logo.svg" width={200} height={200} alt="logo" />
+					<div className={about.indicatorRoot}>
+						<div className={about.indicator}></div>
+					</div>
+					<span className={about.name}>{name}</span>
+					<a className={about.link} href="https://github.com/lochungtin">@enigmaoffline</a>
+				</div>
+			</div>
+
+			<h1><span>ABO</span>UT</h1>
+
+			<p className={about.intro}>Hi, I am <span className={about.highlight}>Timothy</span> Lo</p>
+			<p className={about.text}>I am a <span>PhD student</span> currently researching at UCL alongside Leo Cancer Centre to further develop techniques and methods for upright radiotherapy for the thorax region.</p>
+			<h2>Artifical Intelligence</h2>
+			<p className={about.text}>I currently specialise in the field of <span>computer vision</span> and <span>advanced image analysis</span>. My previous research during my masters degree is to develop an end to end, <span>image to diagnosis pipeline</span> for ultrasonic scan images of ovarian masses. My current research is closely related to my previous as it also requires extensive work on <span>analysing anatomical structures</span> of the chest region.</p>
+			<h2>Software Engineering</h2>
+			<p className={about.text}>On top of being a research on AI and ML, I am also a <span>self-taught web and mobile developer</span>. I specialise in the react family of frameworks, specifically <span>ReactJS</span> and <span>NextJS</span> for web development and React Native for mobile development. Visit my GitHub page or highlighted projects for more of my work.</p>
+			<h2>Hobbies & Robotics</h2>
+			<p className={about.text}>I have always taken an interest in <span>embedded systems</span> and <span>micro controllers</span>. In fact, I was first exposed to programming through robotics. I have created home automation systems with esp32s and arduino boards. I have also built a NAS system using the raspberry pi 4.</p>
+		</section>
+	);
+}
 
 const Skills = () => {
 	return (
 		<section>
 			<h1><span>SKI</span>LLS</h1>
 
-			<div className={layout.row}>
+			<div className={layout.row + " " + layout.skill}>
 				<SkillCard icon="python" name="Python" score="5" />
 				<SkillCard icon="numpy" name="NumPy" score="4" />
 			</div>
-			<div className={layout.row}>
+			<div className={layout.row + " " + layout.skill}>
 				<SkillCard icon="pytorch" name="PyTorch" score="4" />
 				<SkillCard icon="pandas" name="Pandas" score="4" />
 			</div>
-			<div className={layout.row}>
+			<div className={layout.row + " " + layout.skill}>
 				<SkillCard icon="typescript" name="TypeScript" score="4" />
 				<SkillCard icon="javascript" name="JavaScript" score="5" />
 			</div>
-			<div className={layout.row}>
+			<div className={layout.row + " " + layout.skill}>
 				<SkillCard icon="nextjs" name="NextJS" score="4" />
 				<SkillCard icon="react" name="ReactJS" score="5" />
 			</div>
-			<div className={layout.row}>
+			<div className={layout.row + " " + layout.skill}>
 				<SkillCard icon="cplusplus" name="C++" score="3" />
 				<SkillCard icon="arduino" name="Arduino" score="4" />
 			</div>
@@ -36,44 +68,48 @@ const Projects = () => {
 		<section>
 			<h1><span>PRO</span>JECTS</h1>
 
-			<ProjectCard
-				otitle="Ultrasound Auto-Seg"
-				sub="Auto-Segmentation of medical scans"
-				icon1="python"
-				icon2="numpy"
-				icon3="pytorch"
-				cTitle="Auto-Seg"
-				text="(auto-segmentation) of ultrasonic scans for ovarian masses is my main research focus for my Master's thesis. My diagnosis pipline managed to achieved an accuracy of 92%."
-				gh=""
-			/>
-			<ProjectCard
-				otitle="CoinControl"
-				sub="Money Management App"
-				icon1="typescript"
-				icon2="react"
-				icon3="firebase"
-				cTitle="CoinControl"
-				text="is a money management mobile app with an account system, cloud backup, analytics, and also a built-in calculator for easy calculations for recording cashflow"
-				gh="https://github.com/lochungtin/coincontrol"
-			/>
-			<ProjectCard
-				otitle="Expedite"
-				sub="Puzzle Game Solver"
-				icon1="python"
-				icon2="cplusplus"
-				icon3="javascript"
-				cTitle="Project Expedite"
-				text="is my attempt to create solutions, algorithms, or solvers to classic puzzle games. A few principles are setup and all the algorithms abide by these principles."
-				gh=""
-			/>
-			<ProjectCard
-				otitle="RL Pacman"
-				sub="Undergraduate Project"
-				icon1="python"
-				cTitle="RL Pacman"
-				text="is my undergraduate disseration. The topic is to use the Pacman environment to study the differences of classic reinforcement learning and genetic algorithm."
-				gh=""
-			/>
+			<div className={layout.row}>
+				<ProjectCard
+					otitle="Ultrasound Auto-Seg"
+					sub="Auto-Segmentation of medical scans"
+					icon1="python"
+					icon2="numpy"
+					icon3="pytorch"
+					cTitle="Auto-Seg"
+					text="(auto-segmentation) of ultrasonic scans for ovarian masses is my main research focus for my Master's thesis. My diagnosis pipline managed to achieved an accuracy of 92%."
+					gh=""
+				/>
+				<ProjectCard
+					otitle="CoinControl"
+					sub="Money Management App"
+					icon1="typescript"
+					icon2="react"
+					icon3="firebase"
+					cTitle="CoinControl"
+					text="is a money management mobile app with an account system, cloud backup, analytics, and also a built-in calculator for easy calculations for recording cashflow"
+					gh="https://github.com/lochungtin/coincontrol"
+				/>
+			</div>
+			<div className={layout.row}>
+				<ProjectCard
+					otitle="Expedite"
+					sub="Puzzle Game Solver"
+					icon1="python"
+					icon2="cplusplus"
+					icon3="javascript"
+					cTitle="Project Expedite"
+					text="is my attempt to create solutions, algorithms, or solvers to classic puzzle games. A few principles are setup and all the algorithms abide by these principles."
+					gh=""
+				/>
+				<ProjectCard
+					otitle="RL Pacman"
+					sub="Undergraduate Project"
+					icon1="python"
+					cTitle="RL Pacman"
+					text="is my undergraduate disseration. The topic is to use the Pacman environment to study the differences of classic reinforcement learning and genetic algorithm."
+					gh=""
+				/>
+			</div>
 		</section>
 	);
 }
@@ -124,30 +160,34 @@ const Achievements = () => {
 		<section>
 			<h1><span>ACH</span>IEVEMENTS</h1>
 
-			<AchievementCard
-				date="November 2020"
-				title="Hack Sheffield"
-				sub="Multi-award-winning Hackathon Project"
-				text="Our team built a working android mobile app with React Native in 24 hours and won best financial hack and best google cloud hack in the 6th Hack Sheffield hackathon."
-			/>
-			<AchievementCard
-				date="Summer 2018"
-				title="MITIA Summer Camp"
-				sub="Entrepreneurship Summer Camp"
-				text="I was selected to join a highly intensive week summer camp hosted by the MIT Innovative Academy and MIT Innovation Node in Hong Kong."
-			/>
-			<AchievementCard
-				date="May 2017"
-				title="18th Annual Robofest"
-				sub="Double Robotics World Championship"
-				text="Our team was awarded World Championship and the People's Choice Award in the 18th annual Robofest Global Robotics Art Festival in 2017."
-			/>
-			<AchievementCard
-				date="Summer 2015"
-				title="HK Maths Olympiad"
-				sub="Modelling Paper Championship"
-				text="Our team was awarded first place in the Hong Kong Junior Maths Olympiad Modeling Paper for a paper on calculating the day of week given an arbitrary date."
-			/>
+			<div className={layout.row}>
+				<AchievementCard
+					date="November 2020"
+					title="Hack Sheffield"
+					sub="Multi-award-winning Hackathon Project"
+					text="Our team built a working android mobile app with React Native in 24 hours and won best financial hack and best google cloud hack in the 6th Hack Sheffield hackathon."
+				/>
+				<AchievementCard
+					date="Summer 2018"
+					title="MITIA Summer Camp"
+					sub="Entrepreneurship Summer Camp"
+					text="I was selected to join a highly intensive week summer camp hosted by the MIT Innovative Academy and MIT Innovation Node in Hong Kong."
+				/>
+			</div>
+			<div className={layout.row}>
+				<AchievementCard
+					date="May 2017"
+					title="18th Annual Robofest"
+					sub="Double Robotics World Championship"
+					text="Our team was awarded World Championship and the People's Choice Award in the 18th annual Robofest Global Robotics Art Festival in 2017."
+				/>
+				<AchievementCard
+					date="Summer 2015"
+					title="HK Maths Olympiad"
+					sub="Modelling Paper Championship"
+					text="Our team was awarded first place in the Hong Kong Junior Maths Olympiad Modeling Paper for a paper on calculating the day of week given an arbitrary date."
+				/>
+			</div>
 		</section>
 	);
 }
@@ -164,6 +204,7 @@ export default function Home() {
 	return (
 		<>
 			<main>
+				<About name={"TIMOTHY"} />
 				<Skills />
 				<Projects />
 				<Education />
